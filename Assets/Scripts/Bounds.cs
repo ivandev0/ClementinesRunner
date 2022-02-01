7504bf0eea90632cb9a8be366f272dd2ce340525
Ivan Kylchik <cilcic.v22@gmail.com>
2022-02-02 02:18:24 +0300

    Implement ability to fire

---
+++ Assets/Scripts/Bounds.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        Destroy(col.gameObject);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float jumpForce = 400f;
+    public float bulletForce = 800f;
     public LayerMask groundLayer;
+    public GameObject bullet;
+    public Transform firePosition;
 
     private new Rigidbody2D rigidbody2D;
     private bool grounded = true;
@@ -15,9 +18,14 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Update() {
-        if (Input.GetKeyDown("space") && grounded) {
+        if (!grounded) return;
+
+        if (Input.GetKeyDown("space")) {
             grounded = false;
             rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+        } else if (Input.GetMouseButtonDown(0)) {
+            var obj = GameObject.Instantiate(bullet, firePosition.position, bullet.transform.rotation);
+            obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(bulletForce, 0f));
         }
     }
 
