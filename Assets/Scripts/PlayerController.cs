20ad7449d4f7bede1b9a0ee702f4a6a692a1a670
Ivan Kylchik <cilcic.v22@gmail.com>
2022-02-02 20:26:17 +0300

    Add timer to count till next possibility to fire

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float jumpForce = 400f;
     public float bulletForce = 800f;
+    public float deltaFire = 1.0f;
     public LayerMask groundLayer;
     public GameObject bullet;
     public Transform firePosition;
 
     private new Rigidbody2D rigidbody2D;
     private bool grounded = true;
+    private bool canFire = true;
 
     void Start() {
         rigidbody2D = GetComponent<Rigidbody2D>();
@@ -23,9 +25,11 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKeyDown("space")) {
             grounded = false;
             rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-        } else if (Input.GetMouseButtonDown(0)) {
+        } else if (Input.GetMouseButtonDown(0) && canFire) {
+            canFire = false;
             var obj = GameObject.Instantiate(bullet, firePosition.position, bullet.transform.rotation);
             obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(bulletForce, 0f));
+            StartCoroutine(CountTillNextFire());
         }
     }
 
@@ -33,5 +37,10 @@ public class PlayerController : MonoBehaviour {
         if (1 << col.gameObject.layer == groundLayer) {
             grounded = true;
         }
+    }
+
+    private IEnumerator CountTillNextFire() {
+        yield return new WaitForSeconds(deltaFire);
+        canFire = true;
     }
 }
