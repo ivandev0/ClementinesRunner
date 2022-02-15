ab574dbb0853fd7090056ae1414d8e8adc31c2d6
Ivan Kylchik <cilcic.v22@gmail.com>
2022-02-15 14:24:02 +0300

    Tune enemies difficulty and patterns

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,10 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour {
+[System.Serializable]
+public class EnemiesByLevel {
     public GameObject[] enemies;
     public Transform[] spawnPoints;
-    public GameObject[] obstacles;
+}
+
+[System.Serializable]
+public class Patterns {
+    public GameObject[] enemies;
+    public Transform[] spawnPoints;
+    public float delay;
+}
+
+public class EnemySpawner : MonoBehaviour {
+    public EnemiesByLevel[] enemiesByLevel;
+    public Patterns[] patterns;
     public float firstDelay = 2f;
     public float spawnRate = 2f;
 
@@ -24,13 +36,20 @@ public class EnemySpawner : MonoBehaviour {
 
         yield return new WaitForSeconds(firstDelay);
         while (GameManager.Instance.GameIsOn()) {
-            var index = rnd.Next(enemies.Length + obstacles.Length);
-            if (index < enemies.Length) {
+            if (GameManager.Instance.gameLevel == GameManager.Instance.maxGameLevel && rnd.NextDouble() >= 0.5f) {
+                var pattern = patterns[rnd.Next(patterns.Length)];
+                var enemies = pattern.enemies;
+                var spawnPoints = pattern.spawnPoints;
+                for (var i = 0; i < pattern.enemies.Length; i++) {
+                    Instantiate(enemies[i], spawnPoints[i].position, enemies[i].transform.rotation, spawnPosition);
+                    yield return new WaitForSeconds(pattern.delay);
+                }
+            } else {
+                var enemies = enemiesByLevel[GameManager.Instance.gameLevel].enemies;
+                var spawnPoints = enemiesByLevel[GameManager.Instance.gameLevel].spawnPoints;
+                var index = rnd.Next(enemies.Length);
                 var enemy = enemies[index];
                 Instantiate(enemy, spawnPoints[index].position, enemy.transform.rotation, spawnPosition);
-            } else {
-                var obstacle = obstacles[enemies.Length - index];
-                Instantiate(obstacle, transform.position, obstacle.transform.rotation, spawnPosition);
             }
 
             yield return new WaitForSeconds(spawnRate);
